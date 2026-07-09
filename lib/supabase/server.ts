49b050ac4242fd683904a46daed52c78b645e7e1
Carter Smotherman <carterjsmotherman@gmail.com>
2026-07-08 23:39:44 -0400

    supabase

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,7 +1,20 @@
-import { createClient } from "@supabase/supabase-js";
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
 
-export const createServerClient = () =>
-  createClient(
+export async function createClient() {
+  const cookieStore = await cookies();
+
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set() { },
+        remove() { },
+      },
+    }
   );
+}
