e6f7a5db69a789ff41377d3ec784399be0335f63
Carter Smotherman <56828535+csmotherman@users.noreply.github.com>
2026-07-19 10:52:12 -0400

    Enforce canonical non-www domain

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["10.0.0.188"],
+    allowedDevOrigins: ["10.0.0.188"],
 
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-    ],
-  },
+    async redirects() {
+        return [
+            {
+                source: "/:path*",
+                has: [
+                    {
+                        type: "host",
+                        value: "www.playsplitsecond.com",
+                    },
+                ],
+                destination: "https://playsplitsecond.com/:path*",
+                permanent: true,
+            },
+        ];
+    },
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
